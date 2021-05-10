c7cf27aa4d780e4bd586c9fce81572515eaa7f9b
Ubuntu <ubuntu@ip-172-31-57-171.ec2.internal>
2021-05-10 18:08:13 +0000

    added num_guess.py & constants.py

---
+++ constants.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Created by: Layla Michel
+# Created on: May 10, 2021
+# This file contains constants
+
+CORRECT_GUESS = 2
---
+++ num_guess.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by: Layla Michel
+# Created on: May, 10, 2021
+# This program asks the user to input a number between 0 and 9 and
+# displays a message depending on if the guess is correct or wrong
+
+import constants
+
+number = int(input("Guess what number I am thinking of between 0 and 9: "))
+
+
+def main():
+    if (number == constants.CORRECT_GUESS):
+        print("You guessed correctly!")
+    if (number != constants.CORRECT_GUESS):
+        print("You guessed wrong")
+
+
+if __name__ == "__main__":
+    main()
